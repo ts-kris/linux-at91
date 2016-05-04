da1c801541eb4885b7a3636027b999b0d30139df
Ludovic Desroches <ludovic.desroches@atmel.com>
2016-05-04 16:09:16 +0200

    dmaengine: at_xdmac: align descriptors on 64 bits
    Aligning descriptors on 64 bits allows update CNDA and CUBC in an atomic way.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@atmel.com>

--- drivers/dma/at_xdmac.c
+++ drivers/dma/at_xdmac.c
@@ -250,7 +250,7 @@ struct at_xdmac_desc {
 	unsigned int			xfer_size;
 	struct list_head		descs_list;
 	struct list_head		xfer_node;
-};
+}__aligned(sizeof(u64));
 
 static inline void __iomem *at_xdmac_chan_reg_base(struct at_xdmac *atxdmac, unsigned int chan_nb)
 {
