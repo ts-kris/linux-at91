cac2ca99055a6c89448f9728767afd89862f9ce3
Ludovic Desroches <ludovic.desroches@microchip.com>
2018-04-10 12:04:56 +0300

    net: macb: fix for suspend-to-mem feature
    When suspending the macb, the management port for the MIIM interface was
    disabled. When resuming, the PHY (KSZ8081RNB) was no longer responding.
    We have to keep the management port enable to fix this issue.
    
    Moreover, to get an up-to-date backup of the NCFGR register, it has to
    be done just before suspending the macb.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    [claudiu.beznea@microchip.com: removed code from macb_suspend()]
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -2028,14 +2028,23 @@ static void macb_reset_hw(struct macb *bp)
 {
 	struct macb_queue *queue;
 	unsigned int q;
+	unsigned long ctrl;
+
+	/*
+	 * Be careful to not disable port management, it seems that some
+	 * PHYs don't like it.
+	 */
+	ctrl = macb_readl(bp, NCR);
 
 	/* Disable RX and TX (XXX: Should we halt the transmission
 	 * more gracefully?)
 	 */
-	macb_writel(bp, NCR, 0);
+	ctrl &= ~(MACB_BIT(RE) | MACB_BIT(TE));
+	macb_writel(bp, NCR, ctrl);
 
 	/* Clear the stats registers (XXX: Update stats first?) */
-	macb_writel(bp, NCR, MACB_BIT(CLRSTAT));
+	ctrl |= MACB_BIT(CLRSTAT);
+	macb_writel(bp, NCR, ctrl);
 
 	/* Clear all status flags */
 	macb_writel(bp, TSR, -1);
