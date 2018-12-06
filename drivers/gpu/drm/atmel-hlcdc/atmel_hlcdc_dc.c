b6227e637708ec2d2ee8ee808e7795ac2b1cbffe
Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>
2018-12-05 17:51:25 -0700

    drm/atmel-hlcdc: enable sys_clk during initalization.
    for sam9x60 soc, sys_clk is through lcd_gclk clock source and this
    needs to be enabled before enabling lcd_clk.
    
    Signed-off-by: Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>

--- drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_dc.c
+++ drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_dc.c
@@ -734,10 +734,16 @@ static int atmel_hlcdc_dc_load(struct drm_device *dev)
 	dc->hlcdc = dev_get_drvdata(dev->dev->parent);
 	dev->dev_private = dc;
 
+	ret = clk_prepare_enable(dc->hlcdc->sys_clk);
+	if (ret) {
+		dev_err(dev->dev, "failed to enable sys_clk\n");
+		goto err_destroy_wq;
+	}
+
 	ret = clk_prepare_enable(dc->hlcdc->periph_clk);
 	if (ret) {
 		dev_err(dev->dev, "failed to enable periph_clk\n");
-		goto err_destroy_wq;
+		goto err_periph_sysclk_disable;
 	}
 
 	pm_runtime_enable(dev->dev);
@@ -778,6 +784,8 @@ static int atmel_hlcdc_dc_load(struct drm_device *dev)
 err_periph_clk_disable:
 	pm_runtime_disable(dev->dev);
 	clk_disable_unprepare(dc->hlcdc->periph_clk);
+err_periph_sysclk_disable:
+	clk_disable_unprepare(dc->hlcdc->sys_clk);
 
 err_destroy_wq:
 	destroy_workqueue(dc->wq);
@@ -803,6 +811,7 @@ static void atmel_hlcdc_dc_unload(struct drm_device *dev)
 
 	pm_runtime_disable(dev->dev);
 	clk_disable_unprepare(dc->hlcdc->periph_clk);
+	clk_disable_unprepare(dc->hlcdc->sys_clk);
 	destroy_workqueue(dc->wq);
 }
 
@@ -966,6 +975,7 @@ static int atmel_hlcdc_dc_drm_suspend(struct device *dev)
 	regmap_read(regmap, ATMEL_HLCDC_IMR, &dc->suspend.imr);
 	regmap_write(regmap, ATMEL_HLCDC_IDR, dc->suspend.imr);
 	clk_disable_unprepare(dc->hlcdc->periph_clk);
+	clk_disable_unprepare(dc->hlcdc->sys_clk);
 
 	return 0;
 }
@@ -975,6 +985,7 @@ static int atmel_hlcdc_dc_drm_resume(struct device *dev)
 	struct drm_device *drm_dev = dev_get_drvdata(dev);
 	struct atmel_hlcdc_dc *dc = drm_dev->dev_private;
 
+	clk_prepare_enable(dc->hlcdc->sys_clk);
 	clk_prepare_enable(dc->hlcdc->periph_clk);
 	regmap_write(dc->hlcdc->regmap, ATMEL_HLCDC_IER, dc->suspend.imr);
 
