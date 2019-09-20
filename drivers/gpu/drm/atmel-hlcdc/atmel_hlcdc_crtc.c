ee5ce450e06733a9c4974971316fdab69c076165
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-09-20 12:36:24 +0300

    drm/atmel-hlcdc: fix pixel clock divider selection
    Comparison w/ a 10 times discriminator might be too much. We may end up
    choosing a divider which goes to a result frequency to high departed
    from the mode_rate. Choose the divisor which result in a frequency
    closest to the mode_rate (either it is higher or lower than the mode_rate).
    
    Fixes: f6f7ad323461 ("drm/atmel-hlcdc: allow selecting a higher pixel-clock than requested")

--- drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_crtc.c
+++ drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_crtc.c
@@ -127,14 +127,8 @@ static void atmel_hlcdc_crtc_mode_set_nofb(struct drm_crtc *c)
 	} else {
 		int div_low = prate / mode_rate;
 
-		if (div_low >= 2 &&
-		    ((prate / div_low - mode_rate) <
-		     10 * (mode_rate - prate / div)))
-			/*
-			 * At least 10 times better when using a higher
-			 * frequency than requested, instead of a lower.
-			 * So, go with that.
-			 */
+		if (div_low >= 2 && abs(prate / div_low - mode_rate) <
+		    abs(prate / div - mode_rate))
 			div = div_low;
 	}
 
