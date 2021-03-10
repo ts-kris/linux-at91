2a775149aa309a7d6ed421f0455868237269b48f
Eugen Hristev <eugen.hristev@microchip.com>
2021-03-10 14:44:12 +0200

    media: atmel: atmel-isc: add his_entry to register offsets
    Add his_entry to the reg offsets struct.
    This will allow different products to have a different reg offset for this
    particular module.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- drivers/media/platform/atmel/atmel-isc.h
+++ drivers/media/platform/atmel/atmel-isc.h
@@ -153,6 +153,7 @@ struct isc_ctrls {
  * @his:		Offset for the HIS related registers
  * @dma:		Offset for the DMA related registers
  * @version:		Offset for the version register
+ * @his_entry:		Offset for the HIS entries registers
  */
 struct isc_reg_offsets {
 	u32 csc;
@@ -163,6 +164,7 @@ struct isc_reg_offsets {
 	u32 his;
 	u32 dma;
 	u32 version;
+	u32 his_entry;
 };
 
 /*
